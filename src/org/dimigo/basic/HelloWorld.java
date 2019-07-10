e44ae82237c1954f138668a20a830970ad19346b
junsung0207 <junsung0207@naver.com>
2019-07-10 09:23:18 +0900

    안녕하세요 추가

--- src/org/dimigo/basic/HelloWorld.java
+++ src/org/dimigo/basic/HelloWorld.java
@@ -5,5 +5,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello WOrld~!");
         System.out.println("헬로우 월드 !");
+        System.out.println("안녕하세요~~");
     }
 }
